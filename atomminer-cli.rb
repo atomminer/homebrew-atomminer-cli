3f5592794395ffee360088b32fd007daacc9e970
AtomMiner <atom@atomminer.com>
2022-02-15 06:14:20 +0300

    Update atomminer-cli.rb

--- atomminer-cli.rb
+++ atomminer-cli.rb
@@ -2,7 +2,7 @@ class AtomminerCli < Formula
   desc "AM01 mining software"
   homepage "https://atomminer.com"
   url "https://static.atomminer.com/software/dev/atomminer-latest.tar.gz"
-  version "1.0.3RC9"
+  version "1.0.3RC10"
   sha256 "27c5f5b6363c15275c4e3aec1de08ca001b06d66be9021a6514f3f6f3fc7f524"
   license :cannot_represent
 
